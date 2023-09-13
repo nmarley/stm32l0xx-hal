b2b4d3a94e3a6dd423349ac6e8d142732d023c49
Nathan Marley <nathan.marley@gmail.com>
2023-09-13 18:44:50 -0500

    ngm: Set up blinky w/user LED nucleo L053R8

--- examples/blinky.rs
+++ examples/blinky.rs
@@ -2,34 +2,36 @@
 #![no_main]
 #![no_std]
 
-extern crate panic_halt;
-
 use cortex_m_rt::entry;
+use panic_rtt_target as _;
+use rtt_target::{rprintln, rtt_init_print};
 use stm32l0xx_hal::{pac, prelude::*, rcc::Config};
 
 #[entry]
 fn main() -> ! {
+    rtt_init_print!();
+    rprintln!("Moie, welt! STM32 L053R8T6 pilled?");
     let dp = pac::Peripherals::take().unwrap();
+    let cp = cortex_m::Peripherals::take().unwrap();
 
     // Configure the clock.
     let mut rcc = dp.RCC.freeze(Config::hsi16());
+
+    // Get the delay provider.
+    let mut delay = cp.SYST.delay(rcc.clocks);
 
     // Acquire the GPIOA peripheral. This also enables the clock for GPIOA in
     // the RCC register.
     let gpioa = dp.GPIOA.split(&mut rcc);
 
-    // Configure PA1 as output.
-    let mut led = gpioa.pa1.into_push_pull_output();
+    // Configure PA5 as output.
+    let mut led = gpioa.pa5.into_push_pull_output();
 
     loop {
-        // Set the LED high one million times in a row.
-        for _ in 0..1_000_000 {
-            led.set_high().unwrap();
-        }
+        led.set_high().unwrap();
+        delay.delay_ms(1000_u16);
 
-        // Set the LED low one million times in a row.
-        for _ in 0..1_000_000 {
-            led.set_low().unwrap();
-        }
+        led.set_low().unwrap();
+        delay.delay_ms(1000_u16);
     }
 }
